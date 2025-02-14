92988afddd009ae80f933ef60ac5be65f43c9ce3
nizarade <nizaradekurniawan@gmail.com>
2025-02-14 09:18:28 +0700

    Perbaikan Header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
         whileInView={{opacity: 1, y: 0}}
         viewport={{once: true}}
         className='container mx-auto text-center py-4 px-6 md:px-20 lg:px-32 text-white'>
-            <h2 className='text-5xl sm:text-6xl md:text-[82px] font-semibold inline-block max-w-3xl pt-20'>Explore homes that fit your Dreams.</h2>
+            <h2 className='text-5xl sm:text-6xl md:text-[82px] font-semibold inline-block max-w-3xl pt-20'>Explore Hampers that fit your Dreams.</h2>
             <div className='space-x-6 mt-6'>
                 <SecondButton href="#Project">Projects</SecondButton>
                 <Button href="#Contact">Contact Us</Button>
